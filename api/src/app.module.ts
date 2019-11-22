8d62ebbd070036b096424dd86db8d9b8d17b2c92
Unknown <chamayou.guilhem13@gmail.com>
2019-11-22 14:57:53 +0100

    .

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { join } from 'path';
-import { GraphQLModule } from '../lib';
+import { GraphQLModule } from '@nestjs/graphql';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
